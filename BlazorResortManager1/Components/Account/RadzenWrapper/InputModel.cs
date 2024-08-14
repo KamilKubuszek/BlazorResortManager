33cfa6dc8b4f07c71869581def9d5e95f40ae6ea
kamil kubuszek <kamilkubuszek@gmail.com>
2024-08-14 13:14:20 +0200

    obsolete classes

---
+++ BlazorResortManager1/Components/Account/RadzenWrapper/InputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorResortManager1.Components.Account.RadzenWrapper
+{
+    //public class InputModel
+    //{
+    //    [Required]
+    //    //[EmailAddress]
+    //    public string UserName { get; set; } = "";
+
+    //    [Required]
+    //    [DataType(DataType.Password)]
+    //    public string Password { get; set; } = "";
+
+    //    [Display(Name = "Remember me?")]
+    //    public bool RememberMe { get; set; }
+    //}
+}
