b38ad1c8ffe178e869b9a7e20e4ff604185821ad
kamil kubuszek <kamilkubuszek@gmail.com>
2024-08-27 23:17:25 +0200

    migrations add whitelist

---
+++ BlazorResortManager1/Data/Migrations/20240823112813_add-website-whitelist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorResortManager1.Migrations
+{
+    /// <inheritdoc />
+    public partial class addwebsitewhitelist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "widgetWhitelistedUrl",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    siteUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_widgetWhitelistedUrl", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "widgetWhitelistedUrl");
+        }
+    }
+}
---
+++ BlazorResortManager1/Data/Models/Admin/WidgetWhitelistedUrl.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorResortManager1.Data.Models.Admin
+{
+    [Table("widgetWhitelistedUrl")]
+    public class WidgetWhitelistedUrl
+    {
+        [Key]
+        public Guid id { get; set; }
+
+        [Required]
+        public string siteUrl { get; set; }
+    }
+}
