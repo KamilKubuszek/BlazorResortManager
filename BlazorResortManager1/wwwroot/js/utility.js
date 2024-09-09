2fca3dcb0d0dbfa903eb83897791aaaec6ba809b
kamil kubuszek <kamilkubuszek@gmail.com>
2024-09-09 23:11:22 +0200

    add dynamic resizing

---
+++ BlazorResortManager1/wwwroot/js/utility.js
@@ -0,0 +1,27 @@
+﻿function sizeHeightToBottom(elementId) {
+    //get the element
+    var targetElement = document.getElementById(elementId);
+
+    var windowHeight = window.innerHeight;
+    //console.log(windowHeight);
+    var contHoffset = targetElement.offsetTop;
+    //console.log(contHoffset);
+    targetElement.style.height = (windowHeight - contHoffset - 88) + "px";
+}
+
+function getBottomOffsetReference(elementId) {
+    var targetElement = document.getElementById(elementId);
+    return targetElement.offsetTop + targetElement.offsetHeight;
+}
+function sizeHeightToBottomOfParent(elementId, bottomOffset) {
+    //get the element
+    var targetElement = document.getElementById(elementId);
+    //console.log(windowHeight);
+    var contHoffset = targetElement.offsetTop;
+    //console.log(contHoffset);
+    targetElement.style.height = (bottomOffset - contHoffset) + "px";
+}
+
+function alertThing() {
+    alert("alerting test");
+}
